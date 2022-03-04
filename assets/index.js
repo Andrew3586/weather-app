c8000fe01a1f43d53b062267af60443c253d734c
Andrew <andrew_3586@iprimus.com.au>
2022-03-04 14:23:01 +1100

    started javascript

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,14 @@
 let weather = { 
     "apiKey": "9b1f679911ac53cd9b7ab5c58e255fce"
 }
+
+
+const result = fetch ("https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=9b1f679911ac53cd9b7ab5c58e255fce") 
+
+result.then((response)=>{
+    console.log(response)
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
