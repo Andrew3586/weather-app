d02cd30ebea099b4d1a927db8282e3d527cdbddf
Andrew <andrew_3586@iprimus.com.au>
2022-03-11 12:56:08 +1100

    finalising JS coding

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,26 @@
 let weather = { 
-    "apiKey": "9b1f679911ac53cd9b7ab5c58e255fce"
+    "apiKey": "9b1f679911ac53cd9b7ab5c58e255fce",
+fetchWeather: function (city) {
+    fetch (
+    "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey
+    )
+
+.then((response) => response.json())
+.then((data) => this.displayWeather(data))      
+},
+displayWeather: function(data) {
+    const { name } = data
+    const { icon, description } = data.weather[0]
+    const { temp, humidity } = data.main
+    const { speed } = data.wind
+    console.log(name,icon,description,temp,humidity,speed)
+    document.querySelector(".city").innertext = "weather in" + name
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+    document.querySelector(".description").innertext = description
+    document.querySelector(".temp").innertext = temp + "°C"
+    document.querySelector(".humidity").innertext = "humidity" + humidity + "%"
+    document.querySelector(".wind").innertext = "wind speed:" + speed + "km/ph"
+
+
 }
-
-
-const result = fetch ("https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=9b1f679911ac53cd9b7ab5c58e255fce") 
-
-result.then((response)=>{
-    console.log(response)
-    return response.json()
-})
-.then((data)=>{
-    console.log(data)
-})
+}
